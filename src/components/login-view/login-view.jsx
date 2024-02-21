e644d3d8f88eda22bf9066d11aaed667b63b7979
Alex Lantz <aclantz321@gmail.com>
2024-02-20 20:05:33 -0500

    import loginView and SignupView and add token management

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { useState } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault(); //prevent default behavior of form to reload entire page
+    const data = {
+      access: username,
+      secret: password,
+    };
+
+    fetch("https://movie-api-project24-2fb853d4fde0.herokuapp.com/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Login response: ", data);
+        if (data.user) {
+          onLoggedIn(data.user, data.token);
+        } else {
+          alert("No such user");
+        }
+      })
+      .catch((e) => {
+        alert("Something went wrong");
+      });
+
+      if (data.user) {
+        localStorage.setItem("user", JSON.stringify(data.user));
+        localStorgae.setItem("token", data.token);
+      } else {
+        alert("No such user")
+      };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label htmlFor="">
+        Password:
+        <input
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+
+// https://movie-api-project24-2fb853d4fde0.herokuapp.com/login
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,13 +1,29 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+<<<<<<< Updated upstream
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+  const storedToken = localStorage.getItem('token');
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+=======
+  const [user, setUser] = useState(null);
+>>>>>>> Stashed changes
 
   useEffect(() => {
-    fetch("https://movie-api-project24-2fb853d4fde0.herokuapp.com/movies")
+    if (!token) {
+      return;
+    }
+
+    fetch("https://movie-api-project24-2fb853d4fde0.herokuapp.com/movies", {
+      header: { Authorization: "Bearer ${token}" },
+    })
       .then((response) => response.json())
       .then((data) => {
         const moviesFromApi = data.map((doc) => {
@@ -19,33 +35,66 @@ export const MainView = () => {
         });
         setMovies(moviesFromApi);
       });
-  }, []);
+  }, [token]);
 
-  //with bonus task 2 section 3.4
-  if (selectedMovie) {
-    let similarMovies = movies.filter(movie.genre)
+  if (!user) {
+<<<<<<< Updated upstream
     return (
-      <>
-       <MovieView
+      <loginView
+        onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }}
+      />
+    );
+  }
+
+  //Single movie view
+  if (selectedMovie) {
+    return (
+=======
+    return <LoginView onLoggedIn={(user) => setUser(user)} />
+  }
+  
+
+  if (selectedMovie) {
+    return (
+>>>>>>> Stashed changes
+      <MovieView
         movie={selectedMovie}
         onBackClick={() => setSelectedMovie(null)}
       />
-      <hr />
-      <h2>Similar Movies</h2>
-      {similarMovies.map((movie) => {   
-        return (
-          <MovieCard
-          key={movie.id}
-          movie={movie}
-          onMovieClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
-          }}
-        />
-        )
+    );
+  }
+<<<<<<< Updated upstream
+=======
 
-      })}
-    </>
-  )};
+  //bonus task 2 section 3.4
+  // if (selectedMovie) {
+  //   let similarMovies = movies.filter(movie.genre)
+  //   return (
+  //     <>
+  //      <MovieView
+  //       movie={selectedMovie}
+  //       onBackClick={() => setSelectedMovie(null)}
+  //     />
+  //     <hr />
+  //     <h2>Similar Movies</h2>
+  //     {similarMovies.map((movie) => {   
+  //       return (
+  //         <MovieCard
+  //         key={movie.id}
+  //         movie={movie}
+  //         onMovieClick={(newSelectedMovie) => {
+  //           setSelectedMovie(newSelectedMovie);
+  //         }}
+  //       />
+  //       )
+
+  //     })}
+  //   </>
+  // )};
+>>>>>>> Stashed changes
 
   //if array is empty
   if (movies.length === 0) {
@@ -65,6 +114,18 @@ export const MainView = () => {
           />
         );
       })}
+<<<<<<< Updated upstream
+      <button
+        onClick={() => {
+          setUser(null);
+          setToken(null);
+          localStorage.clear();
+        }}>
+        Logout
+      </button>
+=======
+      <button onClick={() => { setUser(null); }}>Logout</button>
+>>>>>>> Stashed changes
     </div>
   );
 };
