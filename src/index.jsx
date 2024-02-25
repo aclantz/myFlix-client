fe7d0e3f4ed0e0f5b798e98df363d3ea72179b0c
Alex Lantz <aclantz321@gmail.com>
2024-02-25 18:30:02 -0500

    add container, import row, pre ternary return

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
-
 import "./index.scss";
+import Container from "react-bootstrap/Container";
 
 // Main component
 const MyFlixApplication = () => {
-  return <MainView />;
+  return (
+  <Container>
+    <MainView />
+  </Container>
+  )
 };
 
 // Finds the root of your app
